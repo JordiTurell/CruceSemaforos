99af41fce42d06c51562563c4163e79df270e854
JordiTurell <jorditn84@hotmail.com>
2020-12-20 17:27:52 +0100

    Inicio

---
+++ Models/Semaforo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semaforos.Models
+{
+    public class Semaforo
+    {
+        public bool red { get; set; }
+        public bool orange { get; set; }
+        public bool green { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Semaforos.Models;
+using System;
+
+namespace Semaforos
+{
+    class Program
+    {
+        Semaforo semaforo1 = new Semaforo();
+        Semaforo semaforo2 = new Semaforo();
+
+        static void Main(string[] args)
+        {
+                
+        }
+    }
+}
